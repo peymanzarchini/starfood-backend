1cb4a9624614e07e1f8227d9c0aaf4baad61ea7b
p.zarchini <p.zarchini@nitro.local>
2025-12-22 09:01:19 +0330

    Writing a project blueprint in the app file

--- server/app.ts
+++ server/app.ts
@@ -1,1 +1,49 @@
-console.log("Hello Starfood");
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+import { responseMidlleware } from "./middlewares/response.js";
+import rateLimit from "express-rate-limit";
+import { errorHandler } from "./middlewares/errorHandler.js";
+import { connnetDB } from "./config/database.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(helmet({ crossOriginResourcePolicy: false }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "10kb" }));
+app.use(cookieParser());
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+
+app.use(responseMidlleware);
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "Too many requests from this IP, please try again after 15 minutes",
+});
+
+app.use("/api", limiter);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  await connnetDB();
+  app.listen(PORT, () => {
+    console.log(`🚀 Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
